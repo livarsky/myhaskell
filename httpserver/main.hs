97518ac130527617cabd77f6573519127dce3cc8
Roman Livarsky <roman1091@gmail.com>
2011-12-18 23:09:22 +0300

    fix

--- httpserver/main.hs
+++ httpserver/main.hs
@@ -1,11 +1,24 @@
 module Main where
 
+import Network
 import System.IO
 import System.Environment
-import Network
-import Directory
 import Control.Concurrent
 import Control.Monad
+import Directory
+
+main :: IO ()
+main = withSocketsDo $ do
+    [port] <- getArgs
+    sock <- listenOn (PortNumber $ fromIntegral (read port :: Int))
+    forever $ do
+        (handle, _, _) <- accept sock
+        forkIO $ do
+            hSetBuffering handle LineBuffering
+            request <- hGetLine handle
+            hSetBuffering handle NoBuffering
+            response request >>= hPutStr handle
+            hClose handle
 
 response :: String -> IO String
 response request = do
@@ -21,17 +34,3 @@ response request = do
 			return $ "HTTP/1.1 200 OK\r\nContent-Length: " ++ (show (length fcontents)) ++ "\r\n\r\n" ++ fcontents
 		else return "HTTP/1.1 404 Not Found\r\n\r\n"
 	else return "HTTP/1.1 400 Bad Request\r\n\r\n"
-
-main :: IO ()
-main = withSocketsDo $ do
-    [port] <- getArgs
-    sock <- listenOn (PortNumber $ fromIntegral (read port :: Int))
-    forever $ do
-        (handle, _, _) <- accept sock
-        forkIO $ do
-            hSetBuffering handle LineBuffering
-            request <- hGetLine handle
-            hSetBuffering handle NoBuffering
-            response request >>= hPutStr handle
-            hClose handle
-
