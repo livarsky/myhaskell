9beedda88eff44875ade5d349a0e4c967dfc2bcc
Roman Livarsky <roman1091@gmail.com>
2012-03-18 14:59:02 +0600

    myreverse IMPROVED!!11

--- bash/myreverse/myreverse.c
+++ bash/myreverse/myreverse.c
@@ -13,7 +13,18 @@ typedef struct buffer buf;
 
 int mread(buf *b) {
     int readed = read(0, b->data + b->stored, b->size - b->stored);
-    return readed >= 0 ? b->stored += readed : -1;
+    b->stored += readed;
+    return readed >= 0 ? readed : -1;
+}
+
+int mwrite(buf *b, int len) {
+    int written = 0;
+    while(written < len) {
+        int tmp_w = write(1, b->data + sizeof(b->data[0]) * written, len - written);
+        if (tmp_w == -1) return -1;
+	    written += tmp_w;
+    }
+    return 0; 
 }
 
 int reverse(buf *b, int len) {
@@ -27,7 +38,7 @@ int reverse(buf *b, int len) {
         end--;
     }
     if (len == 0) return 0; 
-    return write(1, b->data, len + 1);
+    return mwrite(b, len+1);
 }
 
 int findSplitter(buf *b) {
@@ -46,30 +57,33 @@ void remove_seg(buf *b, int end) {
 int kernel() {
     buf b;
     b.size = CAPACITY;
-    b.stored = 0;
-  
+    b.stored = 0; 
     int skip = 0;
     while (1) {
-        if (mread(&b) == -1) return -1;
+        int mr = mread(&b);
+        if (mr == -1) return -1;
         if (b.stored == 0) break;
+        
         int end = findSplitter(&b);
         if (end == b.stored) {     
             if (end == b.size) {
                 skip = 1;
                 b.stored = 0;
-            }
+            } else {
+                if (mr == 0) break;
+            }       
         } else {
-            if (skip) {
-                skip = 0;
-                remove_seg(&b, end + 1);
-            } else {  
-                int rev_res = reverse(&b, end);
-                if (rev_res == -1) return -1;
-                remove_seg(&b, end + 1);
-            }
+                if (skip) {
+                    skip = 0;
+                    remove_seg(&b, end + 1);
+                } else {  
+                    int rev_res = reverse(&b, end);
+                    if (rev_res == -1) return -1;
+                    remove_seg(&b, end + 1);
+                }
         }
-     }
-     return 0;
+    }
+    return 0;
 }
 
 int main() {
